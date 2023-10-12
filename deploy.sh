c723b4cb4a050fddd4396c03ab0bedda7a269aea
Dwenge <shirinkin.denis@bk.ru>
2023-10-12 20:04:03 +0500

    мелочь

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 set -e
 rm -rf dist
-yarn build
-cd dist
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:dwenge/kubaturnik.git master:gh-pages
-cd -
+yarn build &&
+  cd dist &&
+  git init &&
+  git add -A &&
+  git commit -m 'deploy' &&
+  git push -f git@github.com:dwenge/kubaturnik.git master:gh-pages &&
+  cd -
